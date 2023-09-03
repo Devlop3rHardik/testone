615c71cda3044ff95e5a0c2e0301331e6b4c70d8
Devlop3rHardik <143949488+Devlop3rHardik@users.noreply.github.com>
2023-09-03 20:58:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 // Set up scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Create camera controls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.update();
 
-// Position the camera
-camera.position.z = 5;
+// Create a desert-like ground
+const groundMaterial = new THREE.MeshBasicMaterial({ color: 0xd2b48c }); // Sandy brown color
+const groundGeometry = new THREE.PlaneGeometry(20, 20);
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2;
+scene.add(ground);
 
 // Define animation loop
 const animate = () => {
     requestAnimationFrame(animate);
 
-    // Rotate the cube
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // Update camera controls
+    controls.update();
 
     renderer.render(scene, camera);
 };
